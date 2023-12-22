a1f22d43caaba76ec4ccf205df43f139947bfab6
Milly <wmr4job@gmail.com>
2023-12-22 19:10:07 +0800

    add return

--- 20231222_function_parameter.py
+++ 20231222_function_parameter.py
@@ -13,3 +13,10 @@ def add(x=2, y=3): # 參數可以有預設值，寫法左右不用空格
 add() # 執行可不給參數，代入預設值，印出5
 add(10) # 可只給一個，另一個代預設值，印出13
 add(y=5) # 可指定給哪個，印出7
+
+# 3
+def average(numbers):
+	return sum(numbers) / len(numbers) # return回傳結果
+	# numbers是清單，sum可以直接對整個清單做加總
+
+print(average([1,2,3])) # 印出平均是2.0 因為除法會自動轉為float
